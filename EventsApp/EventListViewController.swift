dcea28b90e12ddb4afd6387f9950e7c27d38519d
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-22 21:50:40 +0300

    Create an EventListViewController class

---
+++ EventsApp/EventListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EventListViewController.swift
+//  EventsApp
+//
+//  Created by Ibragim Akaev on 22/05/2021.
+//
+
+import UIKit
+
+final class EventListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
