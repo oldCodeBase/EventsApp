94148482ca89bd4deb35dbbd91c3dbd08d7ed6e0
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-22 22:18:07 +0300

    Create extension for app images

---
+++ EventsApp/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIImage+Extensions.swift
+//  EventsApp
+//
+//  Created by Ibragim Akaev on 22/05/2021.
+//
+
+import UIKit
+
+extension UIImage {
+    static let plusImage = UIImage(systemName: "plus.circle.fill")
+}
