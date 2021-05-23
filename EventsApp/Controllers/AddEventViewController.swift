a0efa5fdf199735473e3f6b9f72b4b2054019e32
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-24 00:38:44 +0300

    SetupUI for a AddEventViewController

--- EventsApp/Controllers/AddEventViewController.swift
+++ EventsApp/Controllers/AddEventViewController.swift
@@ -9,15 +9,89 @@ import UIKit
 
 final class AddEventViewController: UIViewController {
     
+    private let tableView = UITableView()
     var viewModel: AddEventViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        
+        configureVC()
+        configureTableView()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         viewModel.viewDidDisappear()
+    }
+    
+    private func configureVC() {
+        view.backgroundColor = .systemBackground
+        viewModel.onUpdate = { [weak self] in self?.tableView.reloadData() }
+        viewModel.viewDidLoad()
+        navigationItem.title = viewModel.title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .label
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done,
+                                                            target: self,
+                                                            action: #selector(tappedDone))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel,
+                                                           target: self,
+                                                           action: #selector(tappedClose))
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame           = view.bounds
+        tableView.delegate        = self
+        tableView.dataSource      = self
+        tableView.tableFooterView = UIView()
+        tableView.register(NewEventCell.self, forCellReuseIdentifier: "NewEventCellID")
+    }
+    
+    @objc private func tappedDone() {
+        viewModel.tappedDone()
+    }
+    
+    @objc private func tappedClose() {
+        viewModel.tappedClose()
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension AddEventViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRows()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellViewModel = viewModel.cell(for: indexPath)
+        switch cellViewModel {
+        case .titleSubTitle(let titleSubTitleViewModel):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "NewEventCellID", for: indexPath) as! NewEventCell
+            cell.update(with: titleSubTitleViewModel)
+            cell.subtitleTextField.delegate = self
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel.didSelectRow(at: indexPath)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+//MARK: - UITextFieldDelegate
+extension AddEventViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let currentText = textField.text else { return false }
+        let text = currentText + string
+        let point = textField.convert(textField.bounds.origin, to: tableView)
+        if let indexPath = tableView.indexPathForRow(at: point) {
+            viewModel.updateCell(indexPath: indexPath, subtitle: text)
+        }
+        
+        return true
     }
 }
