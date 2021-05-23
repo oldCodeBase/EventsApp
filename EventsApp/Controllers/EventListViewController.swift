410d184b9bef626e9b42dd8f1ba23b75b3f3550e
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-23 10:51:54 +0300

    Refactor code in an EventListViewController

--- EventsApp/Controllers/EventListViewController.swift
+++ EventsApp/Controllers/EventListViewController.swift
@@ -9,23 +9,25 @@ import UIKit
 
 final class EventListViewController: UIViewController {
     
+    var viewModel: EventListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupVC()
     }
     
     private func setupVC() {
-        title                = "Events"
+        title                = viewModel.title
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.tintColor = .primary
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: .plusImage,
                                                            style: .plain,
                                                            target: self,
-                                                           action: #selector(addTapped))
+                                                           action: #selector(tappedAddEventButton))
     }
     
-    @objc private func addTapped() {
-        print("Right bar button tapped")
+    @objc private func tappedAddEventButton() {
+        viewModel.tappedAddEvent()
     }
 }
