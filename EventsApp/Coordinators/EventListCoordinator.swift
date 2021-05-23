50dfec1843054ccc3a7a599388b399379455bad4
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-23 10:51:10 +0300

    Update an EventListCoordinator to handle with childCoordinator

--- EventsApp/Coordinators/EventListCoordinator.swift
+++ EventsApp/Coordinators/EventListCoordinator.swift
@@ -17,8 +17,26 @@ final class EventListCoordinator: Coordinator {
     }
     
     func start() {
-        let eventListViewController = EventListViewController()
+        let eventListViewController       = EventListViewController()
+        let eventListViewModel            = EventListViewModel()
+        eventListViewModel.coordinator    = self
+        eventListViewController.viewModel = eventListViewModel
         navigationController.setViewControllers([eventListViewController],
                                                 animated: false)
+    }
+    
+    func startAddEvent() {
+        let addEventCoordinator = AddEventCoordinator(navigationController: navigationController)
+        childCoordinators.append(addEventCoordinator)
+        addEventCoordinator.parentCoordinator = self
+        addEventCoordinator.start()
+    }
+    
+    func childDidFinish(_ childCoordinator: Coordinator) {
+        if let index = childCoordinators.firstIndex(where: { coordinator in
+            return childCoordinator === coordinator
+        }) {
+            childCoordinators.remove(at: index)
+        }
     }
 }
