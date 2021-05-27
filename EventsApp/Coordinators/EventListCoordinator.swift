173e4839a7432ec48a46525e32877c19ad922ae2
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-27 22:16:04 +0300

    Update an EventListCoordinator

--- EventsApp/Coordinators/EventListCoordinator.swift
+++ EventsApp/Coordinators/EventListCoordinator.swift
@@ -33,6 +33,8 @@ final class EventListCoordinator: Coordinator {
     }
     
     func childDidFinish(_ childCoordinator: Coordinator) {
+        print(CoreDataManager().fetchEvents().first?.name)
+        
         if let index = childCoordinators.firstIndex(where: { coordinator in
             return childCoordinator === coordinator
         }) {
