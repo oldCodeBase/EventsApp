636340d1f0fda1db0c8614a596cb2625da7feb10
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-28 23:52:10 +0300

    Create an EventCellViewModel

---
+++ EventsApp/ViewModels/EventCellViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  EventCellViewModel.swift
+//  EventsApp
+//
+//  Created by Ibragim Akaev on 28/05/2021.
+//
+
+import UIKit
+
+struct EventCellViewModel {
+    
+    var yearText: String {
+        "1 year"
+    }
+    
+    var monthText: String {
+        "2 months"
+    }
+    
+    var weakText: String {
+        "3 weeks"
+    }
+    
+    var dayText: String {
+        "6 days"
+    }
+    
+    var dateText: String {
+        "19 March 2021"
+    }
+    
+    var eventText: String {
+        "Grozny"
+    }
+    
+    var backgroundImage: UIImage {
+        UIImage(systemName: "person")!
+    }
+}
