457be0c34ec025eaad61d871f654c0927377c901
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-29 22:17:31 +0300

    Update code in an EventListViewModel

--- EventsApp/ViewModels/EventListViewModel.swift
+++ EventsApp/ViewModels/EventListViewModel.swift
@@ -10,9 +10,25 @@ import Foundation
 final class EventListViewModel {
     
     let title = "Events"
+    var onUpdate = {}
     var coordinator: EventListCoordinator?
+    enum Cell { case event(EventCellViewModel) }
+    private(set) var cells: [Cell] = []
+    
+    func viewDidLoad() {
+        cells = [.event(EventCellViewModel()), .event(EventCellViewModel())]
+        onUpdate()
+    }
     
     func tappedAddEvent() {
         coordinator?.startAddEvent()
+    }
+    
+    func numberOfRows() -> Int {
+        return cells.count
+    }
+    
+    func cell(at indexPath: IndexPath) -> Cell {
+        return cells[indexPath.row]
     }
 }
