4b483494d7553f382364a09b002b3c0e54f68028
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-31 23:40:27 +0300

    Add a new method into a Date+Extensions class

--- EventsApp/Extensions/Date+Extensions.swift
+++ EventsApp/Extensions/Date+Extensions.swift
@@ -14,4 +14,11 @@ extension Date {
         dateFormatter.dateFormat = "MMM d, yyyy"
         return dateFormatter.string(from: self)
     }
+    
+    func timeRemaining(until endDate: Date) -> String? {
+        let dateComponentsFormatter = DateComponentsFormatter()
+        dateComponentsFormatter.allowedUnits = [.year, .month, .weekOfMonth, .day]
+        dateComponentsFormatter.unitsStyle = .full
+        return dateComponentsFormatter.string(from: self, to: endDate)
+    }
 }
