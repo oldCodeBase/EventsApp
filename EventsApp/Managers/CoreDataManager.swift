6e4b886253dd5a70fdac78806a1f359a251c4681
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-05-31 23:39:48 +0300

    Update a CoreDataManager

--- EventsApp/Managers/CoreDataManager.swift
+++ EventsApp/Managers/CoreDataManager.swift
@@ -10,6 +10,8 @@ import UIKit
 
 final class CoreDataManager {
     
+    static let shared = CoreDataManager()
+    
     lazy var persistentContainer: NSPersistentContainer = {
         let persistentContainer = NSPersistentContainer(name: "EventsApp")
         persistentContainer.loadPersistentStores { _, error in
