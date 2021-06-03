48cacfe4c4b4cc1731615948a443048e92c061bf
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-06-03 22:46:10 +0300

    Create new method for UIImage to increase UI speed

--- EventsApp/Extensions/UIImage+Extensions.swift
+++ EventsApp/Extensions/UIImage+Extensions.swift
@@ -9,4 +9,13 @@ import UIKit
 
 extension UIImage {
     static let plusImage = UIImage(systemName: "plus.circle.fill")
+    
+    func sameAspectRatio(newHeight: CGFloat) -> UIImage {
+        let scale = newHeight / size.height
+        let newWidth = size.width * scale
+        let newSize = CGSize(width: newWidth, height: newHeight)
+        return UIGraphicsImageRenderer(size: newSize).image { _ in
+            self.draw(in: .init(origin: .zero, size: newSize))
+        }
+    }
 }
