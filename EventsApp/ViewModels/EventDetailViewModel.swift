ec6c23f867fae5b26a879cfbe7537732fdaf9c65
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-06-05 23:32:01 +0300

    Update the EventDetailViewModel

--- EventsApp/ViewModels/EventDetailViewModel.swift
+++ EventsApp/ViewModels/EventDetailViewModel.swift
@@ -12,10 +12,24 @@ final class EventDetailViewModel {
     private let eventID: NSManagedObjectID
     private let coreDataManager: CoreDataManager
     private var event: Event?
+    private let date = Date()
     var onUpdate = {}
+    var coordinator: EvenDetailCoordinator?
+    
     var image: UIImage? {
         guard let imageData = event?.image else { return nil }
         return UIImage(data: imageData)
+    }
+    
+    var timeRemainingViewModel: TimeRemainingViewModel? {
+        guard
+            let eventDate = event?.date,
+            let timeRemainingParts = date.timeRemaining(until: eventDate)?.components(separatedBy: ",")
+        else {
+            return nil
+        }
+        
+        return TimeRemainingViewModel(timeRemainingParts: timeRemainingParts, mode: .cell)
     }
     
     init(eventID: NSManagedObjectID, coreDataManager: CoreDataManager = .shared) {
@@ -26,6 +40,10 @@ final class EventDetailViewModel {
     func viewDidLoad() {
         event = coreDataManager.getEvent(eventID)
         onUpdate()
+    }
+    
+    func viewDidDisappear() {
+        coordinator?.didFinish()
     }
 }
 
