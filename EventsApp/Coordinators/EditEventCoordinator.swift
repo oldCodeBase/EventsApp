230b935664dbc268a92c4645c2b210da9294bc3e
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-06-06 22:31:22 +0300

    Create an EventUpdatingCoordinator

---
+++ EventsApp/Coordinators/EditEventCoordinator.swift
@@ -0,0 +1,64 @@
+//
+//  EditEventCoordinator.swift
+//  EventsApp
+//
+//  Created by Ibragim Akaev on 06/06/2021.
+//
+
+import UIKit
+
+protocol EventUpdatingCoordinator: Coordinator {
+    var onUpdateEvent: () -> Void { get }
+}
+
+final class EditEventCoordinator: Coordinator {
+    
+    private(set) var childCoordinators: [Coordinator] = []
+    private let navigationController: UINavigationController
+    private let event: Event
+    private var completion: (UIImage) -> Void = { _ in }
+    
+    var parentCoordinator: EventUpdatingCoordinator?
+    
+    init(event: Event, navigationController: UINavigationController) {
+        self.event = event
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let editEventViewController = EditEventViewController()
+        let editEventViewModel = EditEventViewModel(event: event, cellBuilder: EventCellBuilder())
+        editEventViewModel.coordinator = self
+        editEventViewController.viewModel = editEventViewModel
+        navigationController.pushViewController(editEventViewController, animated: true)
+    }
+    
+    func didFinish() {
+        parentCoordinator?.childDidFinish(self)
+    }
+    
+    func didFinishUpdateEvent() {
+        parentCoordinator?.onUpdateEvent()
+        navigationController.popViewController(animated: true)
+    }
+    
+    func showImagePicker(completion: @escaping (UIImage) -> Void) {
+        self.completion = completion
+        let imagePickerCoordinator = ImagePickerCoordinator(navigationController: navigationController)
+        imagePickerCoordinator.parentCoordinator = self
+        imagePickerCoordinator.onFinishPicking = { [weak self] image in
+            completion(image)
+            self?.navigationController.dismiss(animated: true, completion: nil)
+        }
+        childCoordinators.append(imagePickerCoordinator)
+        imagePickerCoordinator.start()
+    }
+    
+    func childDidFinish(_ childCoordinator: Coordinator) {
+        if let index = childCoordinators.firstIndex(where: { coordinator -> Bool in
+            return childCoordinator === coordinator
+        }) {
+            childCoordinators.remove(at: index)
+        }
+    }
+}
