04e0da467fecbd022d38645fc7009894dd631b06
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-06-06 22:35:58 +0300

    Update an EventListCoordinator

--- EventsApp/Coordinators/EventListCoordinator.swift
+++ EventsApp/Coordinators/EventListCoordinator.swift
@@ -8,45 +8,44 @@
 import UIKit
 import CoreData
 
-final class EventListCoordinator: Coordinator {
-    
+final class EventListCoordinator: EventUpdatingCoordinator {
     private(set) var childCoordinators: [Coordinator] = []
+    var onUpdateEvent = { }
+    
     private let navigationController: UINavigationController
-    var onSaveEvent = {}
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
     
     func start() {
-        let eventListViewController       = EventListViewController()
-        let eventListViewModel            = EventListViewModel()
-        eventListViewModel.coordinator    = self
-        onSaveEvent                       = eventListViewModel.reload
+        let eventListViewController = EventListViewController()
+        let eventListViewModel = EventListViewModel()
+        eventListViewModel.coordinator = self
+        onUpdateEvent = eventListViewModel.reload
         eventListViewController.viewModel = eventListViewModel
-        navigationController.setViewControllers([eventListViewController],
-                                                animated: false)
+        navigationController.setViewControllers([eventListViewController], animated: false)
     }
     
     func startAddEvent() {
         let addEventCoordinator = AddEventCoordinator(navigationController: navigationController)
-        childCoordinators.append(addEventCoordinator)
         addEventCoordinator.parentCoordinator = self
+        childCoordinators.append(addEventCoordinator)
         addEventCoordinator.start()
     }
     
+    func onSelect(_ id: NSManagedObjectID) {
+        let eventDetailCoordinator = EventDetailCoordinator(eventID: id, navigationController: navigationController)
+        eventDetailCoordinator.parentCoordinator = self
+        childCoordinators.append(eventDetailCoordinator)
+        eventDetailCoordinator.start()
+    }
+    
     func childDidFinish(_ childCoordinator: Coordinator) {
-        if let index = childCoordinators.firstIndex(where: { coordinator in
+        if let index = childCoordinators.firstIndex(where: { coordinator -> Bool in
             return childCoordinator === coordinator
         }) {
             childCoordinators.remove(at: index)
         }
-    }
-    
-    func onSelect(_ id: NSManagedObjectID) {
-        let eventDetailCoordinator = EvenDetailCoordinator(eventID: id, navigationController: navigationController)
-        eventDetailCoordinator.parentCoordinator = self
-        childCoordinators.append(eventDetailCoordinator)
-        eventDetailCoordinator.start()
     }
 }
